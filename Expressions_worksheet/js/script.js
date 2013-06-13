d0b6575a8ca2a08096beadf8c1d7f03f88cdac31
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-13 00:07:50 -0500

    Commiting Expressions Worksheet: problem 1.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+//Expression Worksheet Problem 1: Dog Years
+
+
+//Defining Dog's Age
+var dogAge = 7;
+
+//Definfing Sparky's Actual Age
+var sparkyActualAge = 3;
+
+//Calculating Sparky's age in Dog years.
+var sparkyDogAge = dogAge*sparkyActualAge
+console.log(sparkyDogAge);
+
+
+
+
