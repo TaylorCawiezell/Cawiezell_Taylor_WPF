20863bea0c959ca6f98234a13041fd6fdaada850
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-13 22:21:00 -0500

    Final subission for Expressions assignment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,13 +5,13 @@ alert("BestBuy has many employees and a huge work enviorment. Lets find out in c
 // Im calculating the number of total square footage and deviding it by the number of employees.
 
 var bestBuy = prompt("How many BestBuy stores are there in our calculation?");
-
+// calculating the size of the store
 var size = prompt("How many square feet does each store have?");
-
+//calculating the number of employees
 var employee = prompt("How many employees are in each store?");
-
+//calculating the squre foot total of stores
 var totalSQ = bestBuy * size;
-
+//expression for how much square feet for each employee
 var totalPE = bestBuy * size / employee;
 
 alert("Out of " + totalSQ + " total square feet there are " + totalPE + " square feet per emloyee.");
