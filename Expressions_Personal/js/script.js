9c3112661560ffeb1daae3283f0e30b498c073b4
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-13 20:34:27 -0500

    Commit changes to assignmnet expressions.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,8 @@
+// Taylor Cawiezell 6/13/2013: Assignment Expressions
 
+//How many hours of video games I should play in a lifetime. 
+alert("How many hours of video games do you think I should play in a lifetime?");
+
+var hoursD = promt("How many days should I play a week?")
+
+var hours
