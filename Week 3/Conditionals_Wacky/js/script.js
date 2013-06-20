bca1c7a749b070e6683acb6ef61c413c6c476333
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-20 14:27:55 -0500

    Finished Conditional wacky

--- Week 3/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Wacky/js/script.js
@@ -1,8 +1,14 @@
-var age = 6;
-var book;
+// Taylor Cawiezell - Conditionals Assignment - Wacky - 06/20/2013
 
-book = (age < 10) ? "green eggs and ham" : "the time machine";
-console.log(book);
+//variable for if the woodchuck could chuck wood
+alert("Lets find out if the woodchuck can chuck wood.")
+var wood = prompt("How many pounds of wood did this woodchuck atempt to chuck.");
+var dirt = 700;
+
+// Conditional for the WoodChuck.
+woodChucked = (wood < dirt)
+              ? alert("The WoodChuck chucked the wood.") 
+              : alert("The WoodChuck did not chuck wood.");
 
 
 
