17f515b3ce141ac698d7c7b96c210ce915c98a6c
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-20 13:56:24 -0500

    Finished Personal conditionals assignment

--- Week 3/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Personal/js/script.js
@@ -1,8 +1,13 @@
-var age = 6;
-var book;
+// Taylor Cawiezell - Conditionals Assignment - Personal - 06/20/2013
 
-book = (age < 10) ? "green eggs and ham" : "the time machine";
-console.log(book);
+//How many Poke'mon I've collected versus many friends I have
+alert("Lets see how many friends Taylor can keep after buying the new Poke'mon game.")
+// variables for pokemon collected and friends I currently have.
+var friends = prompt("How many friends does Taylor have?");
+var pokemon = prompt("How many Poke'mon has Taylor Collected?")
+// Condition for having any friends left.
+ friendsLeft = (friends < pokemon) ? alert("Taylor lost all his friends.") : prompt("Taylor has " + (friends - pokemon) + " friends left.");
+
 
 
 
