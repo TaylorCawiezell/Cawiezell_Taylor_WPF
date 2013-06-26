77d73b148088758e3486a835d562823e5b6502f7
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2013-06-26 15:04:56 -0500

    Completion of Functions Worksheet for week 4.

---
+++ Week 4/Functions_worksheet/js/script.js
@@ -0,0 +1,23 @@
+//functions work sheet- Cawiezell_Taylor_Function Worksheet  6/26/2013
+
+
+//STUNG!
+
+
+// giving the funtion its parameter.
+function numberOfstings(weight, sting ){
+	var death = weight * sting;
+    console.log(death);
+};
+// testing functions aurgument.
+numberOfstings(1, 8.666666667);
+numberOfstings(10, 8.666666667);
+numberOfstings(100, 8.666666667);
+numberOfstings(1000, 8.666666667);
+numberOfstings(10000, 8.666666667);
+
+
+
+	
+	
+	
